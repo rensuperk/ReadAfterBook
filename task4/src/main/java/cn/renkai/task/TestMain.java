616864a1480f1125f2e16656c2b9a480b1c5db1c
renkai <304078213@qq.com>
2017-03-18 20:44:05 +0800

    push

---
+++ task4/src/main/java/cn/renkai/task/TestMain.java
@@ -0,0 +1,11 @@
+package cn.renkai.task;
+
+/**
+ * Created by renkai on 2017/3/3.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("删除了就好了吗");
+    }
+
+}
